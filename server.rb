5f5967360b5cb9b5eaa94b70ce282c84fcc20e98
Masayoshi Wada <developer@andantesoftware.com>
2015-12-20 03:28:51 +0900

    add cors settings and crossorigin.xml

--- server.rb
+++ server.rb
@@ -3,14 +3,21 @@ require 'rack/cors'
 require 'erubis'
 require 'json'
 
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '/register', :headers => :any, :methods => :post
+  end
+end
+
 set :erb, :escape_html => true
 
 # {holder: name, point: num}
-@@score_board = []
+$score_board = []
 
 helpers do
   def register_score(name, point)
-    @@score_board << {
+    $score_board << {
       holder: name,
       point: point
     }
@@ -18,13 +25,18 @@ helpers do
 end
 
 get '/' do
-  erb :index, locals: { score_board: @@score_board }
+  erb :index, locals: { score_board: $score_board }
 end
 
 post '/register' do
   register_score(params[:name], params[:point])
   content_type :json
   { success: true }.to_json
+end
+
+get '/crossdomain.xml' do
+  content_type :xml
+  erb :crossdomain
 end
 
 __END__
@@ -44,3 +56,10 @@ __END__
   <% end %>
 </body>
 </html>
+
+@@ crossdomain
+<cross-domain-policy xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.adobe.com/xml/schemas/PolicyFile.xsd">
+  <site-control permitted-cross-domain-policies="master-only"/>
+  <allow-access-from domain="scratchx.org"/>
+  <allow-access-from domain="llk.github.io"/>
+</cross-domain-policy>
